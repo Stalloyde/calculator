27512579be3aa004df8d41dae38b93ec6d9f7f0c
Stalloyde <stalloyde93@gmail.com>
2022-07-06 13:47:04 +0800

    Add eventlisteners on function buttons - Enabled second round of digit input after clicking function button

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const digitButtons = document.querySelectorAll(".digit-buttons");
-const functionButtons = document.querySelectorAll(".function-Buttons")
+const functionButtons = document.querySelectorAll(".function-buttons")
 const display = document.querySelector(".display");
 const displayValue1 = [];
 const displayValue2 = [];
 
 digitButtons.forEach(item => item.addEventListener("click", function () {
     display.textContent += this.textContent;
-    displayValue1.push(display.textContent);
-}));   
+    displayValue1.push(display.textContent); 
+}))   
 
+// the display.textContent being retrieve contains displayValue1. need to clear the display.textContent after clicking '+'
+functionButtons.forEach(item => item.addEventListener("click", function () {
+    display.textContent = "";
+    displayValue2.push(display.textContent);
+})); 
+
+
+//click '+'
+// take final element of displayValue1
+// activates function sum()... 
+// activates displayValue2
+//
 
 function sum (displayValue1, displayValue2) {
     return displayValue1 + displayValue2;
