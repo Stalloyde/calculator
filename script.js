0ab4c279ec8b5aa4fffd918a17b201f7a4569ea0
Stalloyde <stalloyde93@gmail.com>
2022-07-07 19:22:43 +0800

    Add functioning calculator (for one equation)
    - WIP: Use solution of first equation to roll over to next equation

--- script.js
+++ script.js
@@ -1,38 +1,91 @@
 const digitButtons = document.querySelectorAll(".digit-buttons");
 const functionButtons = document.querySelectorAll(".function-buttons")
+const clearButton = document.getElementById("clear");
+const deleteButton = document.getElementById("delete");
+const percentButton = document.getElementById("percent");
+const divideButton = document.getElementById("divide");
+const multiplyButton = document.getElementById("multiply");
+const sumButton = document.getElementById("sum");
+const subtractButton = document.getElementById("subtract");
+const decimalButton = document.getElementById("decimal");
+const equalsButton = document.getElementById("equals");
 const display = document.querySelector(".display");
 const displayValue1 = [];
 const displayValue2 = [];
+const operatorValue = [];
+let functionButtonClicked = false;
 
-//problem is the data storage of first and second wave of input.
+function divide (finalValue1, finalValue2) {
+    return finalValue1 / finalValue2;
+}
+
+function multiply (finalValue1, finalValue2) {
+    return finalValue1 * finalValue2;
+}
+    
+function sum (finalValue1, finalValue2) {
+    return finalValue1 + finalValue2;
+}
+
+function subtract (finalValue1, finalValue2) {
+    return finalValue1 - finalValue2;
+}    
+
+function operator (operator, num1, num2) {
+    if (operator === "divide") {
+        return divide(num1, num2);
+    } else if 
+        (operator === "multiply") {
+        return multiply(num1, num2);
+    } else if 
+        (operator === "sum") {
+        return sum(num1, num2);
+    } else if 
+        (operator === "subtract") {
+        return subtract(num1, num2);
+    };
+}
+
 digitButtons.forEach(item => item.addEventListener("click", function () {
     display.textContent += this.textContent;
-    displayValue1.push(display.textContent); 
-    displayValue2.push(display.textContent);
+    (functionButtonClicked === true) ? displayValue2.push(display.textContent) : displayValue1.push(display.textContent);
 }))   
 
-functionButtons.forEach(item => item.addEventListener("click", function () {
+divideButton.addEventListener("click", function () {
+    functionButtonClicked = true;
     display.textContent = "";
-    //haven't added function sum/subract/multiple/divide
-})); 
+    operatorValue.push("divide");
+})
+
+multiplyButton.addEventListener("click", function () {
+    functionButtonClicked = true;
+    display.textContent = "";
+    operatorValue.push("multiply");
+})
+
+sumButton.addEventListener("click", function () {
+    functionButtonClicked = true;
+    display.textContent = "";
+    operatorValue.push("sum");
+})
+
+subtractButton.addEventListener("click", function () {
+    functionButtonClicked = true;
+    display.textContent = "";
+    operatorValue.push("subtract");
+}) 
+
+equalsButton.addEventListener("click", function () {
+    display.textContent = "";
+    const finalValue1 = Number(displayValue1.slice(-1));
+    const finalValue2 = Number(displayValue2.slice(-1));
+    const finalOperator = operatorValue.toString();
+    display.textContent += operator(finalOperator, finalValue1, finalValue2);
+
+ })
 
 
-function sum (displayValue1, displayValue2) {
-    return displayValue1 + displayValue2;
-}
 
-function subtract (displayValue1, displayValue2) {
-    return displayValue1 - displayValue2;
-}
 
-function multiply (displayValue1, displayValue2) {
-    return displayValue1 * displayValue2;
-}
 
-function divide (displayValue1, displayValue2) {
-    return displayValue1 / displayValue2;
-}
-
-function operate (operator) {
-   return operator;
-}
+    
