db40ad8590a72eea94c4473db863ee1926ec0f88
Stalloyde <stalloyde93@gmail.com>
2022-07-13 09:48:52 +0800

    Reset variables after an operation is completed

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const displayValue1 = [];
 const displayValue2 = [];
 const operatorValue = [];
 let functionButtonClicked = false;
+let equalsButtonClicked = false;
+
 
 function divide (finalValue1, finalValue2) {
     return finalValue1 / finalValue2;
@@ -47,10 +49,8 @@ function operator (operator, num1, num2) {
 }
 
 function getSolution () {
-    display.textContent = "";   
     const solutionValue = operator(operatorValue.toString(), Number(displayValue1.slice(-1)), Number(displayValue2.slice(-1)));
-    display.textContent +=  solutionValue;
-    functionButtonClicked = false;
+    display.textContent += solutionValue;
     displayValue1.length = 0;
     displayValue2.length = 0;
     operatorValue.length = 0;
@@ -62,38 +62,70 @@ function resetDisplay () {
     display.textContent = "";
 }
 
+function reset () {
+    display.textContent = "";
+    displayValue1.length = 0;
+    displayValue2.length = 0;
+    operatorValue.length = 0;
+    functionButtonClicked = false;
+}
+
+//why arent my numbers concatenating on each other??
 digitButtons.forEach(item => item.addEventListener("click", function () {
     display.textContent += this.textContent;
     (functionButtonClicked === true) ? displayValue2.push(display.textContent) : displayValue1.push(display.textContent);
+
+    if (equalsButtonClicked === true) {
+        reset();
+        equalsButtonClicked = false;
+        display.textContent += this.textContent;
+    }
+    console.log(displayValue1)
+    console.log(displayValue2)
+    console.log(operatorValue)
 }))   
 
 divideButton.addEventListener("click", function () {
     resetDisplay();
+    if (displayValue1.length > 0 && displayValue2.length > 0) {getSolution()};
     operatorValue.push("divide");
+    console.log(displayValue1)
+    console.log(displayValue2)
+    console.log(operatorValue)
 })
 
 multiplyButton.addEventListener("click", function () {
     resetDisplay();
+    if (displayValue1.length > 0 && displayValue2.length > 0) {getSolution()};
     operatorValue.push("multiply");
 })
 
 sumButton.addEventListener("click", function () {
     resetDisplay();
+    if (displayValue1.length > 0 && displayValue2.length > 0) {getSolution()};
     operatorValue.push("sum");
 })
 
 subtractButton.addEventListener("click", function () {
     resetDisplay();
+    if (displayValue1.length > 0 && displayValue2.length > 0) {getSolution()};
     operatorValue.push("subtract");
 }) 
 
-equalsButton.addEventListener("click", getSolution);
+equalsButton.addEventListener("click", function () {
+    functionButtonClicked = false;
+    equalsButtonClicked = true;
+    resetDisplay();
+    getSolution();
+    console.log(displayValue1)
+    console.log(displayValue2)
+    console.log(operatorValue)
+});
 
+// first condition: equals button clicked -> if click on digit, to start new calculation... empty all arrays and reset all functions.
+    // input the new digit clicked into dv1
 
+    //  equals button clicked -> if click on operator, chain operation
 
-//object that contains dV and oV
-//keep adding more dV and oVs in tandem with clicks
-//loop to retrieve?
-
-
-    //click operator button -> if dV1 & dv2 isnt empty, run solutionValue
+// second condition: equals button not clicked. operator button clicked -> solution displayed on screen -> carry over solution number to dV1 
+// -> when clicking next number, empty display screen, display screen show next number, push next number clicked to dV2 
