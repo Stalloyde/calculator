93f6d3d52a0e9cc9e52f9be527331403ab69a103
Stalloyde <stalloyde93@gmail.com>
2022-07-18 11:29:06 +0800

    Refactor codes for digit & operator buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//queryselectors
 const buttons = document.querySelectorAll(".button-items");
 const digitButtons = document.querySelectorAll(".digit-buttons");
 const functionButtons = document.querySelectorAll(".function-buttons")
@@ -20,6 +21,7 @@ let equalsButtonClicked = false;
 let solutionValueCheck = false;
 let digitButtonCheck = false;
 
+//declaring functions
 function divide (finalValue1, finalValue2) {
     return finalValue1 / finalValue2;
 }
@@ -53,7 +55,7 @@ function operator (operator, num1, num2) {
 
 function getSolution () {
     if (displayValue1.length > 0 && displayValue2.length > 0) {
-        const solutionValue = operator(operatorValue.toString(), Number(displayValue1.slice(-1)), Number(displayValue2.slice(-1)));
+        const solutionValue = operator(operatorValue.toString() , Number(displayValue1.slice(-1)), Number(displayValue2.slice(-1)));
         display.textContent += solutionValue;
         displayValue1.length = 0;
         displayValue2.length = 0;
@@ -78,80 +80,77 @@ function inputOperatorValue (operator) {
     if (operatorValue.length === 0) {operatorValue.push(operator)};
 }
 
+function getOperatorValue(operator) {
+    if (digitButtonCheck === true) {
+        display.textContent = "";
+        operatorButtonClicked = true;
+        getSolution();
+        inputOperatorValue(operator);
+        digitButtonCheck = false;
+    };
+}
+
+function getDisplayValue () {
+    (operatorButtonClicked === true) ? displayValue2.push(display.textContent) : displayValue1.push(display.textContent);   
+}
+
+function updateDisplay (content) {
+    //initial round of operations
+    display.textContent += content;
+    getDisplayValue();
+    digitButtonCheck = true;
+
+    if //subsequent rounds of operations via equals button
+    (equalsButtonClicked === true && operatorButtonClicked === false) { 
+        fullReset();
+        display.textContent += content;
+        getDisplayValue();
+        equalsButtonClicked = false;
+        solutionValueCheck = false;
+        digitButtonCheck = true;
+
+    } else if //subsequent rounds of operations via operator buttons
+    (solutionValueCheck === true) { 
+        display.textContent = "";
+        display.textContent += content;
+        getDisplayValue();
+        solutionValueCheck = false;
+        digitButtonCheck = true;
+    };
+}
+
+
+//start of eventlisteners
 buttons.forEach(item => item.addEventListener("click", function () {
     item.classList.add("clicked");
     item.addEventListener("transitionend", function () {item.classList.remove("clicked")});
 }))
 
 digitButtons.forEach(item => item.addEventListener("click", function () {
-    //initial round of operations
-    display.textContent += this.textContent; 
-    (operatorButtonClicked === true) ? displayValue2.push(display.textContent) : displayValue1.push(display.textContent);   
-    digitButtonCheck = true;
-
-    if //subsequent rounds of operations via equals button
-    (equalsButtonClicked === true && operatorButtonClicked === false) { 
-        equalsButtonClicked = false;
-        solutionValueCheck = false;
-        fullReset();
-        display.textContent += this.textContent;
-        (operatorButtonClicked === true) ? displayValue2.push(display.textContent) : displayValue1.push(display.textContent);
-        digitButtonCheck = true;
-} else if //subsequent rounds of operations via operator buttons
-    (solutionValueCheck === true) { 
-        display.textContent = "";
-        display.textContent += this.textContent;
-        displayValue2.push(display.textContent);
-        solutionValueCheck = false;
-        digitButtonCheck = true;
-    };
-}))   
+    updateDisplay(this.textContent);
+}))
 
 divideButton.addEventListener("click", function () {
-    if (digitButtonCheck === true) {
-        display.textContent = "";
-        operatorButtonClicked = true;
-        getSolution();
-        inputOperatorValue("divide");
-        digitButtonCheck = false;
-    };
+    getOperatorValue("divide");
 })
 
 multiplyButton.addEventListener("click", function () {
-    if (digitButtonCheck === true) {
-        display.textContent = "";
-        operatorButtonClicked = true;
-        getSolution();
-        inputOperatorValue("multiply");
-        digitButtonCheck = false;
-    };
+    getOperatorValue("multiply");
 })
 
 sumButton.addEventListener("click", function () {
-    if (digitButtonCheck === true) {
-        display.textContent = "";
-        operatorButtonClicked = true;
-        getSolution();
-        inputOperatorValue("sum");
-        digitButtonCheck = false;
-    };
+    getOperatorValue("sum");
 })
 
 subtractButton.addEventListener("click", function () {
-    if (digitButtonCheck === true) {
-        display.textContent = "";
-        operatorButtonClicked = true;
-        getSolution();
-        inputOperatorValue("subtract");
-        digitButtonCheck = false;
-    };
+    getOperatorValue("subtract");
 }) 
 
 equalsButton.addEventListener("click", function () {
     display.textContent = "";
-    operatorButtonClicked = false;
-    equalsButtonClicked = true;
     getSolution();
+    equalsButtonClicked = true;
+    operatorButtonClicked = false;
 
     if (solutionValueCheck === false) {
     display.textContent = "ERROR";
